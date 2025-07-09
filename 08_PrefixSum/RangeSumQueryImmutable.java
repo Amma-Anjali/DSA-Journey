5a67f9773b9bb6a2fcc0b04dd7043f1650282d8e
Amma-Anjali <ammaanjali578@gmail.com>
2025-07-09 07:15:01 +0530

    Updated RangeSumQueryImmutable.java with leetcode link and comments

--- 08_PrefixSum/RangeSumQueryImmutable.java
+++ 08_PrefixSum/RangeSumQueryImmutable.java
@@ -1,19 +1,38 @@
+/**
+ * LeetCode Problem: 303. Range Sum Query - Immutable
+ * Link: https://leetcode.com/problems/range-sum-query-immutable/
+ *
+ * Description:
+ * Implements a NumArray class using prefix sums to support fast O(1) range sum queries.
+ * The constructor precomputes the prefix sums during initialization.
+ *
+ * Time Complexity:
+ * - Constructor: O(n)
+ * - sumRange: O(1)
+ */
+public class NumArray {
 
-// Description:
-//   Builds a prefix sum array during construction.
-//   Allows fast O(1) range sum queries using the difference of prefix sums.
+    private final int[] prefixSum;
 
-class NumArray {
-
-    private int[] prefixSum;
-
+    /**
+     * Constructor: Builds the prefix sum array.
+     *
+     * @param nums The input array of integers.
+     */
     public NumArray(int[] nums) {
-        prefixSum = new int[nums.length + 1];
-        for(int i = 0; i < nums.length; i++){
+        prefixSum = new int[nums.length + 1]; // 1 extra space for easier indexing
+        for (int i = 0; i < nums.length; i++) {
             prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
     }
 
+    /**
+     * Returns the sum of elements between indices left and right (inclusive).
+     *
+     * @param left  Starting index of the range.
+     * @param right Ending index of the range.
+     * @return The sum from nums[left] to nums[right].
+     */
     public int sumRange(int left, int right) {
         return prefixSum[right + 1] - prefixSum[left];
     }
